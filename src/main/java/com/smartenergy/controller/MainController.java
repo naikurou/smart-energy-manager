6d8e78416e3dd179bf306f17a9ba6e72d5794ee7
Nahil <nahilgameur@gmail.com>
2026-05-26 20:10:20 +0200

    fix: ajout des imports JavaFX manquants dans MainController

--- src/main/java/com/smartenergy/controller/MainController.java
+++ src/main/java/com/smartenergy/controller/MainController.java
@@ -1,7 +1,14 @@
 package com.smartenergy.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
